95d3feae8dad088f21e6764fe739cc4dd4d91098
Junaid Qadir <642353+JunaidQadirB@users.noreply.github.com>
2023-09-21 18:50:58 +0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -87,7 +87,18 @@ html_theme = 'alabaster'
 # documentation.
 #
 html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
     'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
